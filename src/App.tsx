5f873fde6a51b305afbea2e5bc52095c584ff879
susung1107 <susung1107@gmail.com>
2023-06-12 01:01:36 +0900

    충돌수정

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,18 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-<<<<<<< HEAD
-        <Route path="/" element={<Box w="full">Home</Box>} />
-        <Route path="/order" element={<Box w="full">Order</Box>} />
-        <Route path="/about" element={<Box w="full">About</Box>} />
-        <Route path="/shopkeeper" element={<ShopkeeperPage />} />
-=======
         <Route path="/" element={<Main />} />
         <Route path="/order" element={<ShoppingBasketPage />} />
         <Route path="/detail/:id" element={<DetailPage />} />
         <Route path="/mypage" element={<MyPage />} />
-        <Route path="/shopkeeper" element={<Box w="full">Shopkeeper</Box>} />
->>>>>>> c7bc9ec315d2ce5fd62893064feca2554e287703
+        <Route
+          path="/shopkeeper"
+          element={
+            <Box w="full">
+              <ShopkeeperPage />
+            </Box>
+          }
+        />
       </Route>
     </Routes>
   );
